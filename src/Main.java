6914a80bc13f594e94a84ea3297cfae9d4d3425f
reyesjuan12 <git config --global user.name reyesjuan12>
2022-11-15 10:12:42 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        String nombres;
+        int pago;
+        int  dias;
+        String nombre2;
+        int  dias2;
+        int pago2;
+        String nombre3;
+        int  dias3;
+        int pago3;
+        String nombre4;
+        int  dias4;
+        int pago4;
+        String nombre5;
+        int  dias5;
+        int pago5;
+        pago = 700000;
+        double horas;
+        System.out.println("hola usuario  digita tu nombre");
+        nombres = Sc.nextLine();
+        System.out.println("hola buenas digita tu edad");
+        dias = Sc.nextInt();
+        System.out.println("hola usuario porfavor digita horas trabajadas");
+        horas = Sc.nextByte();
+        System.out.println("su nombre es " +nombres+ " bien y su edad es" +dias+ " y sus horas son" +horas+ "");
+        System.out.println("y su pago es " +pago+ "gracias por consultar el siguiente ");
+        System.out.println("hola buenas  digita tu nombre");
+        nombre2 = Sc.nextLine();
+        System.out.println("hola usuario digita tu edad");
+        dias2 = Sc.nextInt();
+        System.out.println("hola usuario digita horas trabajadas");
+        pago2 = Sc.nextByte();
+        System.out.println("su nombre es " +nombre2+ " bien y su edad es" +dias2+ " y sus horas son" );
+        System.out.println("y su pago es " +pago2+ "gracias por consultar el siguiente ");
+        System.out.println("hola buenas digita tu nombre");
+        nombre3 = Sc.nextLine();
+        System.out.println("hola buenas digita tu edad");
+        dias3 = Sc.nextInt();
+        System.out.println("hola buenas digita horas trabajadas");
+        pago3 = Sc.nextByte();
+        System.out.println("su nombre es " +nombre3+ " bien y su edad es" +dias3+ " y sus horas son" );
+        System.out.println("y su pago es " +pago3+ "gracias por consultar el siguiente ");
+        System.out.println("hola buenas digita tu nombre");
+        nombre4 = Sc.nextLine();
+        System.out.println("hola usuario digita tu edad");
+        dias4 = Sc.nextInt();
+        System.out.println("hola usuario digita horas trabajadas");
+        pago4 = Sc.nextByte();
+        System.out.println("su usuario es " +nombre4+ " bien y su edad es" +dias4+ " y sus horas son" );
+        System.out.println("y su pago es " +pago4+ "gracias por consultar el siguiente ");
+        System.out.println("hola usuario digita tu nombre");
+        nombre5 = Sc.nextLine();
+        System.out.println("hola usuario digita tu edad");
+        dias5 = Sc.nextInt();
+        System.out.println("hola usuario digita horas trabajadas");
+        pago5 = Sc.nextByte();
+        System.out.println("su nombre es " +nombre5+ " bien y su edad es" +dias5+ " y sus horas son" );
+        System.out.println("y su sueldo es " +pago5+ "gracias por consultar el siguiente ");
+
+
+
+    }
+
+}
